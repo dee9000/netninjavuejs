f6035d7502743f65ef54b5385a58e40278881598
Dotun Asunmo <dee9000@Dotuns-MacBook-Pro.local>
2023-08-31 21:28:46 +0100

    further vue features added

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+//first project
 const app = Vue.createApp({
     //data, functions
     data() {
@@ -9,16 +10,92 @@ const app = Vue.createApp({
         }
     },
     methods:{
-        changePublisher(){
-            this.publisher = 'penguin'
+        changePublisher(publisher){
+            //this.publisher = 'penguin'
+            this.publisher = publisher
         },
-        changeTitle(){
-            this.title = 'Nemesis of the dark'
+        changeTitle(title){
+            //this.title = 'Nemesis of the dark'
+            this.title = title
         },
-        changeAuthor(){
-            this.author = "Thomas Kale"
+        changeAuthor(author){
+            //this.author = "Thomas Kale"
+            this.author = author
         }
     }
 })
 
 app.mount('#app')
+
+
+//second project
+const app2 = Vue.createApp({
+    data(){
+        return{
+        title: '---',
+        year:1985,
+        genre: 'Adventure',
+        actor: '--'
+        }
+    },
+    methods:{
+        changeTitle(title){
+            this.title = title
+        },
+        changeActor(actor){
+            this.actor = actor
+        }
+    }
+})
+
+app2.mount('#app2')
+
+
+
+//third project
+const app3 = Vue.createApp({
+    data(){
+        return{
+        showStuff:true,
+        title: '--',
+        year:1978,
+        genre: 'Adventure',
+        actor: 'Christopher reeves'
+        }
+    },
+    methods:{
+        changeTitle(title){
+            this.title = title
+        },
+        toggleButton(){
+            this.showStuff = !this.showStuff
+        }
+
+    }
+})
+app3.mount('#app3')
+
+
+
+const app4 = Vue.createApp({
+    data(){
+        return{
+            showStuff:true,
+            title: '--',
+            year:1978,
+            genre: 'Adventure',
+            actor: 'Christopher reeves'
+              }
+    },
+    methods:{
+        handleEvent(){
+            console.log('event')
+        },
+        toggleItem(){
+            this.showStuff = !this.showStuff
+        }
+
+    }
+})
+
+app4.mount('#app4')
